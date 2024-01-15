2741aac90983cdbf9c784a6ebec85d43edcf2a7a
fast8saim <semyon.mityaev@gmail.com>
2024-01-15 08:34:32 +0500

    Versions

--- fast8tube_main.py
+++ fast8tube_main.py
@@ -1,5 +1,11 @@
 import fast8tube_gui
+import sys
 
+VERSION = '2024.01.15.1'
 
 if __name__ == '__main__':
+
+    if len(sys.argv) > 1 and (sys.argv[1] == "--version" or sys.argv[1] == '-v'):
+        print(VERSION)
+
     fast8tube_gui.create_gui()
